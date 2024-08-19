d9cacf1631468fcd22fef920948950aa34958f66
Luís Fernando Parizotto <164382347+LParizotto@users.noreply.github.com>
2024-08-19 14:30:52 -0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         //Declaração de variáveis
-        String produto = "Cerveja";
         int valor = 10;
-        int quant = 12;
         double imposto = 0.2;
         double lucro = 0.2;
 
         //Operações
-        int bruto = valor * quant;
-        double impostototal = bruto * imposto;
-        double valortotal = bruto + impostototal;
         double margemlucro = 1 - lucro;
         double precovenda = valortotal / margemlucro;
         double ganhos = precovenda - valortotal;
@@ -22,9 +17,6 @@ public class Main {
         System.out.println("Imposto: " + imposto);
         System.out.println("Lucro: " + lucro);
 
-        System.out.println("Bruto: " + bruto);
-        System.out.println("Valor com imposto: " + impostototal);
-        System.out.println("Valor total do prduto: " + valortotal);
         System.out.println("Margem de lucro desejada: " + lucro);
         System.out.println("Preço da venda: " + precovenda);
         System.out.println("Ganhos da venda: " + ganhos);
